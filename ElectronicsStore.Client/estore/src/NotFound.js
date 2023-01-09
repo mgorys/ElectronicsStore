0fc04edcba52662e012492730dd6a8825928c5da
gogo <gorycho@interia.pl>
2023-01-09 10:47:29 +0100

    notfound

--- ElectronicsStore.Client/estore/src/NotFound.js
+++ ElectronicsStore.Client/estore/src/NotFound.js
@@ -3,7 +3,10 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h2>tu jest 'NotFound'</h2>
+      <img
+        src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg"
+        style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+      />
     </div>
   );
 };
